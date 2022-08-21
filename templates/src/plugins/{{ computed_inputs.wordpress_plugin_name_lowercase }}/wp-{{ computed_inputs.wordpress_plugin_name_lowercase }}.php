811458cd7d3f9dcb85ae5912b77d924f0a172fa2
Lucas Apoena <lucasapoena@gmail.com>
2022-08-21 16:49:11 -0300

    Plugin :: Ajustando implementação para o wordpress reconhecer o plugin

---
+++ templates/src/plugins/{{ computed_inputs.wordpress_plugin_name_lowercase }}/wp-{{ computed_inputs.wordpress_plugin_name_lowercase }}.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: {{ computed_inputs.wordpress_plugin_name_lowercase }}
+* Plugin URI: {{ inputs.wordpress_plugin_uri }}
+* Description: {{ inputs.wordpress_plugin_description }}.
+* Version: {{ inputs.wordpress_plugin_version }}
+* Author: {{ inputs.wordpress_plugin_author }}
+* Author URI: {{ inputs.wordpress_plugin_author_uri }}
+**/
